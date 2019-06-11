89f7c31af08ef1c2e53305229645a55d8caec503
Lrop <Lagropner04@gmail.com>
2019-06-11 14:40:07 -0600

    saving progress 3

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,125 +1,76 @@
+//Global Attributes
+
+var counter=setInterval(timer, 1000);
+var count= 30;
 let score = 0;
-var count=30;
 let currentQuestion = 0;
-let questions= [
+
+
+
+let questions = [
 {
-    text: "Who does Ricky Marry",
-    answers: ["Lucy", "Rig Driver", "Cheesebrugers"],
-    correct: 1,
+    title: "Who does Ricky Marry",
+    answers: ["lucy", "rig driver", "cheesebrugers", "crepes"],
+    correct: 0
 },
 {
-    text: "Who does Ray Do",
-    answers: ["Lucy", "Rig Driver", "Cheesebrugers"],
-    correct: 2,
+    title: "Who does Ray Do",
+    answers: ["lucy", "rig driver", "cheesebrugers", "crepes"],
+    correct: 1
 
 },
 {
-    text: "Who does Ricky Eat",
-    answers: ["Lucy", "Rig Driver", "Cheesebrugers"],
-    correct: 3,
+    title: "Who does Ricky Eat",
+    answers: ["lucy", "rig driver", "cheesebrugers", "crepes"],
+    correct: 2
 
 },
 {
-    text: "Who does Ricky Marry",
-    answers: ["Lucy", "Rig Driver", "Cheesebrugers"],
-    correct: 4,
+    title: "Who does Ricky Marry",
+    answers: ["lucy", "rig driver", "cheesebrugers", "crepes"],
+    correct: 3
 
 },
-]
+];
+
+
 
 
 $(document).ready(function(){
     $('.start a').click(function(){
         $('.start').hide();
         $('.quiz-section').show();
+        showQuestion();
+        timer();
         
     });
 });
 
-var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
-
-function timer()
-{
-  count=count-1;
-  if (count <= -1)
-  {
-     clearInterval(counter);
-     return;
-     
-  }
-
- document.getElementById("clock").innerHTML=count ; // watch for spelling
+function showQuestion() {
+    let question = questions[currentQuestion];
+    $('.paragraph-question').text(question.title);
+    $('.quiz-section paragraph-selection').html('');
+    console.log(sh)
 }
 
 
-
-
-
-
-
-
-/*var trivia = {
-    correct: 0,
-    incorrect: 0,
-    unasnwered: 0,
-    currentRound: 0,
-    timer: 25,
-    timerOn: false,
-    timerId : '',
-
-    questions: {
-        questionOne: 'Who does Ricky marry?',
-        questionTwo: 'What was Rays job before the accident',
-        questionThree: 'What is Randys favorite food',
-        questionFour: 'Who most resembles a Cave Man',
-        questionFive: 'In series One, who tries to take over the park',
-        questionSix: 'What is the name of Bubbles evil doll',
-        questionSeven: 'How many times was Ricky shot(arrows,bullet,spears,projectiles etc',
-        questionEight: 'Where did Ricky sleep for most of the show',
-        questionNine: 'On average how many F-bombs are said per episode',
-        QuestionTen: 'Rays ends up spending his last few years lving where?',
-    },
-
-    options: {
-        questionOne: ['Lucy','Barbra', 'Sarah', 'Trinity'],
-        questionTwo: ['Hairdresser', 'Did not have one','Rig Driver','Cop'],
-        questionThree: ['Streak', 'Ribs', 'CheeseBurgers', 'Sandwhiches'],
-        questionFour: ['Bubbles', 'Julian', 'Sam Losco', 'Trevor'],
-        questionFive: ['Denis', 'Terry', 'J-ROC', 'Cyrus'],
-        questionSix: ['Conky', 'Chuckie', 'Candy', "Corkie"],
-        questionSeven: ['23', '7', '41', 'Too many to count'],
-        questionEight: ['Car', 'House', 'Shed','His bed duh'],
-        questionNine: ['4', '46', '13','22'],
-        QuestionTen: ['The Dump', 'On the Road', 'With his family', 'Hes stil alive'],
-    },
-
-    answers: {
-        questionOne: 'Lucy',
-        questionTwo: 'Rig Driver',
-        questionThree: 'CheeseBurgers',
-        questionFour: 'Sam Losco',
-        questionFive: 'Cyrus',
-        questionSix: 'Conky',
-        questionSeven:'Too many to count',
-        questionEight: 'Car',
-        questionNine: '46',
-        QuestionTen: 'The Dump',
+function timer() {
+    count=count-1;
+    if (count <= -1)
+    {
+       clearInterval(counter);
+       return;  
     }
-}
+   document.getElementById("clock").innerHTML= count ;
+  }
+   
 
 
 
-function trivia (questions, options, answers) {
-    this.questions = questions;
-    this.options  = options;
-    this.answers = answers;
 
 
-trivia.prototype.correctAnswer = function(choice) {
-    return choice === this.answers;
-}
-}
-*/
+
+
 /*
 var startBtn = document.getElementById('startBtn');
 var count=30;
