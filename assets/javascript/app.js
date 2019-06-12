2c7c12d616c61aec1a7b82b1e035ba4d68a9eea2
Lrop <Lagropner04@gmail.com>
2019-06-12 15:40:47 -0600

    saving progess5/additional functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,70 +1,99 @@
 //Global Attributes
 
-var counter=setInterval(timer, 1000);
-var count= 30;
+var counter = setInterval(timer, 1000);
+var count = 30;
 let score = 0;
 let currentQuestion = 0;
 
 
-
-
-
 let questions = [
-{
-    title: "Who does Ricky Marry",
-    answers: ["lucy","rig driver","cheesebrugers","crepes"],
-    correct: 0
-},
-{
-    title: "Who does Ray Do",
-    answers: ["lucy","rig driver","cheesebrugers","crepes"],
-    correct: 1
+    {
+        title: "Who does Ricky Marry?",
+        answers: ["lucy", "rig driver", "cheesebrugers", "crepes"],
+        correct: 0
+    },
+    {
+        title: "Who does Ray Do?",
+        answers: ["lucy", "rig driver", "cheesebrugers", "crepes"],
+        correct: 1
 
-},
-{
-    title: "Who does Ricky Eat",
-    answers: ["lucy","rig driver","cheesebrugers","crepes"],
-    correct: 2
+    },
+    {
+        title: "Who does Ricky Eat?",
+        answers: ["lucy", "rig driver", "cheesebrugers", "crepes"],
+        correct: 2
 
-},
-{
-    title: "Who does Ricky Marry",
-    answers: ["lucy","rig driver","cheesebrugers","crepes"],
-    correct: 3
+    },
+    {
+        title: "Who does Ricky Marry?",
+        answers: ["lucy", "rig driver", "cheesebrugers", "crepes"],
+        correct: 3
 
-},
+    },
 ];
 
 
 //Functions
 
-$(document).ready(function(){
-    $('.start a').click(function(){
+$(document).ready(function () {
+    $('.start a').click(function () {
         $('.start').hide();
         $('.quiz-section').show();
         showQuestion();
         timer();
-        
+    });
+
+    $('.quiz-section ul').on("click", 'li' , function () {
+        $('.highlight').removeClass('highlight');
+        $(this).addClass('highlight');
+
     });
 });
 
+// Shows the first question
+
 function showQuestion() {
     let question = questions[currentQuestion];
-    $('#paragraph-question').text = (questions.title);
-    $('.quiz-section paragraph-selection').html('');
+    $('#paragraph-question').text(question.title);
+    $('.quiz-section ul').html('');
     console.log(question.title);
+    for (var i = 0; i < question.answers.length; i++) {
+        $('.quiz-section ul').append("<li id='" + i + "'>" + question.answers[i] + "</li>");
+
+    }
 }
 
+
+function showAnswer() {
+    var answer = questions.answer[1];
+    console.log(answer);
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
 // Timer
+
+
+
 function timer() {
-    count=count-1;
-    if (count <= -1)
-    {
-       clearInterval(counter);
-       return;  
+
+    count = count - 1;
+    if (count <= -1) {
+        clearInterval(counter);
+        return;
     }
-    
-   document.getElementById("clock").innerHTML= count ;
-  }
-   
+
+    document.getElementById("clock").innerHTML = count;
+}
+
 
